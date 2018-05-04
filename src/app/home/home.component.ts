0a923a6b94814a580d07f9ea4410dd744f93701b
Rickey Galloway <rickey.galloway@MedSeek.com>
2018-05-04 18:10:59 -0500

    added add/remove functions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,15 @@ import { trigger, style, transition, animate,keyframes,query,stagger } from '@an
                   style({opacity:0, transform: 'translateY(-75%)', offset:0}),
                   style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
                   style({opacity:1, transform: 'translateY(0)', offset:1})
-              ]))]), {optional: true})
+              ]))]), {optional: true}),
+
+              query(":leave", stagger('300ms', [
+                animate('.6s ease-in', keyframes([
+                    style({opacity:1, transform: 'translateY(0)', offset:0}),
+                    style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
+                    style({opacity:0, transform: 'translateY(-75%)', offset:1})
+                ]))]), {optional: true}),
+
       ])
     ])
   ]
@@ -37,5 +45,9 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText= '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);    
   }
 }
